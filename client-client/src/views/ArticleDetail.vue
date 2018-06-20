6d0504dd3f37c7e252cc4b041686b14fc63466bf
faizal <faizalramanda@gmail.com>
2018-06-20 19:56:23 +0700

    75% kerangka client

---
+++ client-client/src/views/ArticleDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Ini Article detail</h1>
+    {{ article }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    article () {
+      return this.$store.state.article.selectedArticle
+    }
+  },
+  beforeCreate () {
+    this.$store.dispatch('article/getArticle', this.$route.params.id)
+      .catch(message => {
+        alert(message)
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client-client/src/views/Services.vue
+++ client-client/src/views/Services.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
     <h1>This is service page</h1>
+    <div class="cards">
+      <div class="card" v-for="(service, index) in services" v-bind:key='index' style="width: 18rem;">
+        <img class="card-img-top" :src="service.image" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{ service.name }}</h5>
+          <p class="card-text">{{ service.description }}</p>
+          <!-- <router-link class="btn btn-primary" :to="{ name: 'article-detail', params: { id: article._id } }">Primary</router-link> -->
+          <!-- <router-link :to="{ name: 'article-detail', params: { id: article._id } }"></router-link> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    services () {
+      return this.$store.state.service.services
+    }
+  },
+  beforeCreate () {
+    this.$store.dispatch('service/getAllService')
+      .catch(message => {
+        alert(message)
+      })
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .cards {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .card {
+    margin: 5px;
+  }
 </style>
